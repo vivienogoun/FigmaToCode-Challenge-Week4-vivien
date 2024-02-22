838cc60e17ed80bca1fbe3d1f3b221617968f690
Vivien Ogoun <vivienogoun1@gmail.com>
2024-02-22 19:19:32 +0100

    fix: resolve errors after corrections

--- components/layout/main-nav.tsx
+++ components/layout/main-nav.tsx
@@ -1,4 +1,3 @@
-import * as React from "react"
 import Link from "next/link"
 import { cn } from "@/utils/shadcn"
 
--- components/skill.tsx
+++ components/skill.tsx
@@ -1,4 +1,5 @@
-import * as React from "react"
+"use client"
+
 import { defaultIconStyle, hoverIconStyle } from "@/utils/constants"
 import { useTheme } from "next-themes"
 
